fed9817fb7b0e80ba5117a31534564c8a0e381f6
vitaliy197811 <vital197811@gmail.com>
2023-03-31 23:01:39 +0300

    structure1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-
 import { StartScreen } from 'pages';
+import RegisterPage from 'pages/RegisterPage';
+import SignInPage from 'pages/SignInPage';
+
 import Favorite from '../pages/Favorite/Favorite';
 import MyRecipes from '../pages/MyRecipes/MyRecipes';
 
@@ -8,8 +10,22 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
-      <Route path="/register" element={<div>register</div>} />
-      <Route path="/login" element={<div>login</div>} />
+      <Route
+        path="/register"
+        element={
+          <PublicRoute restricted>
+            <RegisterPage />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute restricted>
+            <SignInPage />
+          </PublicRoute>
+        }
+      />
       <Route path="/favorite" element={<Favorite />} />
       <Route path="/my" element={<MyRecipes />} />
     </Routes>
