7f7ca0d1a7ac41f1968db1549d775dbfe8368d40
kolya8077 <kolya8077@gmail.com>
2023-04-01 10:49:21 +0300

    Rounded button
    Компонент заокругленої кнопки який можна перевикористовувати

---
+++ src/reusableComponents/Btn/btn.style.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { theme } from "constants/theme";
+
+
+export const Btn = styled(NavLink)`
+  background-color: ${props =>
+    ({
+      normal: theme.colors.dark,
+      dark: theme.colors.accent,
+      transparent: 'transparent',
+    }[props.variant])};
+
+  clip-path: inset(25% 0 25% 0 round 15% 35% 15% 30%);
+  padding: 50px 42px;
+
+  border: none;
+  color: ${theme.colors.white};
+  transition: all 250ms linear;
+
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: ${theme.fontWeights.normal};
+  font-size: 16px;
+  line-height: 24px;
+
+  :hover,
+  :focus {
+    background-color: ${props =>
+      ({
+        normal: theme.colors.accent,
+        dark: theme.colors.dark,
+        transparent: theme.colors.accent,
+      }[props.variant])};
+
+    @media ${theme.device.mobile} {
+      font-size: 14px;
+      line-height: 21px;
+    }
+    @media ${theme.device.tablet} {
+      font-size: 16px;
+    }
+  }
+`;
