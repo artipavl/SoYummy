e0e19e76618779114b0a586e24937a546c3f23c2
vitaliy197811 <vital197811@gmail.com>
2023-04-02 22:33:48 +0300

    squares in title

--- src/components/MainTitle/MainTitle.jsx
+++ src/components/MainTitle/MainTitle.jsx
@@ -1,9 +1,14 @@
-import { Title } from './MainTitle.styled';
+import { Title, Green1, Green2, Black1 } from './MainTitle.styled';
 
 const MainTitle = ({ text }) => {
   return (
-    <Title>{text}</Title>
-  )
+    <Title>
+      {text}
+      <Green1></Green1>
+      <Green2></Green2>
+      <Black1></Black1>
+    </Title>
+  );
 };
 
 export default MainTitle;
--- src/components/MainTitle/MainTitle.styled.js
+++ src/components/MainTitle/MainTitle.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Title = styled.h2`
+  position: relative;
   display: block;
   font-family: 'Poppins';
   color: #001833;
@@ -9,6 +10,7 @@ export const Title = styled.h2`
   font-feature-settings: 'liga' off;
   font-size: 28px;
   line-height: 100%;
+  padding-top: 62px;
 
   @media ${props => props.theme.device.tablet} {
     font-size: 32px;
@@ -17,3 +19,70 @@ export const Title = styled.h2`
     font-size: 44px;
   }
 `
+export const Green1 = styled.div`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  left: 104px;
+  top: 14px;
+  background: #8baa36;
+  border-radius: 3px;
+  transform: rotate(-25deg);
+
+  @media ${props => props.theme.device.tablet} {
+    width: 14px;
+    height: 14px;
+    left: 187px;
+    top: 23px;
+  }
+
+  @media ${props => props.theme.device.desktop} {
+    left: 228px;
+    top: 54px;
+  }
+`
+
+export const Green2 = styled.div`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  left: 333px;
+  top: 46px;
+  background: #8baa36;
+  border-radius: 3px;
+  transform: rotate(-25deg);
+
+  @media ${props => props.theme.device.tablet} {
+    width: 14px;
+    height: 14px;
+    left: 674px;
+    top: 36px;
+  }
+
+  @media ${props => props.theme.device.desktop} {
+    left: 1150px;
+    top: 67px;
+  }
+`
+export const Black1 = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 6px;
+  left: 231px;
+  top: 79px;
+  background: #22252a;
+  border-radius: 3px;
+  transform: rotate(-25deg);
+
+  @media ${props => props.theme.device.tablet} {
+    width: 12px;
+    height: 12px;
+    left: 405px;
+    top: 93px;
+  }
+
+  @media ${props => props.theme.device.desktop} {
+    left: 707px;
+    top: 137px;
+  }
+`;
