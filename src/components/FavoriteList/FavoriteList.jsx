e4f3c8bc293432dc9e824da5da4ecd0fd8f51943
vitaliy197811 <vital197811@gmail.com>
2023-04-02 18:01:13 +0300

    Favorite+MyRecipes

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,24 +1,60 @@
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router';
+import { getFavoriteRecipes } from 'api/index';
 import MyRecipeItem from 'components/MyRecipeItem/MyRecipeItem';
+
 import css from './FavoriteList.module.css';
+import {
+  List,
+  FavoriteListText,
+  // ButtonDelete,
+  // ButtonRecipe,
+} from './FavoriteList.styled.js';
 
 const FavoriteList = () => {
-  const [loading, ] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [allRecipes, setAllRecipes] = useState([]);
+  // const location = useLocation();
+
+  useEffect(() => {
+    const renderMovies = async () => {
+      setLoading(true);
+      try {
+        setAllRecipes(await getFavoriteRecipes());
+        // console.log(setAllRecipes);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    renderMovies();
+  }, []);
 
   return (
-    <div className={css.favoriteList}>
-      <ul className={css.list}>
-        {/* {loading && <Loader />} */}
-
-        {!loading ? (
-              <MyRecipeItem />
-        ) : (
-            <h3>You don't have favorite recipes</h3>
-        )}
-      </ul>
+    <List>
+      {/* {loading && <Loader />} */}
+      {allRecipes.length !== 0 && !loading ? (
+        allRecipes.map(({ description, preview, time, title, _id }) => (
+          <MyRecipeItem
+            key={_id.$oid}
+            description={description}
+            preview={preview}
+            time={time}
+            title={title}
+            id={_id}
+            // handelDelete={handelDelete}
+            ButtonDelete={css.buttonDelete}
+            ButtonRecipe={css.buttonRecipe}
+            // eslint-disable-next-line react/style-prop-object
+            style="normal"
+          />
+        ))
+      ) : (
+        <FavoriteListText>You don't have favorite recipes</FavoriteListText>
+      )}
       {/* { <Paginator /> } */}
-    </div>
+    </List>
   );
 };
 
