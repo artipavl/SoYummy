7640fc007b0d8fde334d998bb30ae26aa2409492
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-04-06 20:28:40 +0300

    lite fix

--- src/components/Head/UserMenu/EditModalUser/EditNameForm/EditNameForm.js
+++ src/components/Head/UserMenu/EditModalUser/EditNameForm/EditNameForm.js
@@ -1,11 +1,9 @@
 import { EditNameFormStyled, EditNameFormWrap } from "./EditNameForm.styled"
-import { UserIconStyled } from "./EditNameForm.styled";
 
-import {useFiel, ErrorMessage} from 'formik'
 
 export const EditNameForm = ({ name, handleChangeName }) => {
 
-  
+
 
   return (
     <EditNameFormWrap>
