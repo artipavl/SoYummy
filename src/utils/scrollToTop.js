3a9c7a7a8eecbac2669607029fdcaf7004b3be18
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-04-11 17:29:46 +0300

    add lazy scroll

--- src/utils/scrollToTop.js
+++ src/utils/scrollToTop.js
@@ -1,11 +1,17 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-
 export const GoToTop = () => {
   const routePath = useLocation();
   const onTop = () => {
-    window.scrollTo(0, 0);
+    const scrollStep = window.scrollY / 25;
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, -scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
   };
   useEffect(() => {
     onTop();
