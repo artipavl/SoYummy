9e66a1e9dfdc85b48a67667c51bff405d39ead03
ElenaObukhova1984 <e.obukhova84@gmail.com>
2023-04-11 16:58:30 +0300

    update Global style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,15 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
+
 export const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
-        /* font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-            sans-serif; */
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue','Poppins',
+            sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        font-family: 'Poppins', sans-serif;
         font-weight: 400;
         font-size: 14px;
         line-height: 18px;
