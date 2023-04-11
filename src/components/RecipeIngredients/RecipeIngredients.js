8dfe3687906e92ecdea10d16393117598a79aba3
Vitalii <svitalii138@gmail.com>
2023-04-11 22:41:57 +0300

    Loader added

--- src/components/RecipeIngredients/RecipeIngredients.js
+++ src/components/RecipeIngredients/RecipeIngredients.js
@@ -8,6 +8,7 @@ import {
   IngredientPlaceholder,
   IngredientsHeader,
   IngredientsSection,
+  LoaderDiv,
   MarginText,
   TitleText,
 } from './RecipeIngredients.styled';
@@ -16,12 +17,15 @@ import Checkbox from 'components/Checkbox/Checkbox';
 import { useEffect, useState } from 'react';
 import { getShopList } from 'api/services/axios/axiosService';
 import { useParams } from 'react-router-dom';
+import { Loader } from 'components/Loader/Loader';
 
 const RecipeIngredients = ({ ingredients }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [shopList, setShopList] = useState([]);
   const { recipeId } = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     getShopList()
       .then(res => {
         const { result } = res.data.data;
@@ -30,47 +34,56 @@ const RecipeIngredients = ({ ingredients }) => {
         );
         setShopList(filteredShopList);
       })
-      .catch(err => console.log(err.message));
+      .catch(err => console.log(err.message))
+      .finally(setIsLoading(false));
   }, [recipeId]);
 
   return (
     <IngredientsSection>
       <Container>
-        <IngredientsHeader>
-          <TitleText>Ingredients</TitleText>
-          <FlexWrapper>
-            <MarginText>Number</MarginText>
-            <TitleText>Add to list</TitleText>
-          </FlexWrapper>
-        </IngredientsHeader>
-        <ul>
-          {ingredients.map(ingr => {
-            return (
-              <IngredientItem key={ingr._id}>
-                <FlexWrapper>
-                  {ingr.thb ? (
-                    <IngredientImg src={ingr.thb} alt={ingr.ttl} />
-                  ) : (
-                    <IngredientPlaceholder>
-                      <use href={placeholders + '#ph-apple-60px'} />
-                    </IngredientPlaceholder>
-                  )}
-                  <IngredientName>{ingr.ttl}</IngredientName>
-                </FlexWrapper>
-                <FlexWrapper>
-                  <IngredientNumber>{ingr.measure}</IngredientNumber>
-                  <label>
-                    <Checkbox
-                      ingrId={ingr._id}
-                      shopListProp={shopList}
-                      ingrMeasure={ingr.measure}
-                    />
-                  </label>
-                </FlexWrapper>
-              </IngredientItem>
-            );
-          })}
-        </ul>
+        {isLoading ? (
+          <LoaderDiv>
+            <Loader />
+          </LoaderDiv>
+        ) : (
+          <>
+            <IngredientsHeader>
+              <TitleText>Ingredients</TitleText>
+              <FlexWrapper>
+                <MarginText>Number</MarginText>
+                <TitleText>Add to list</TitleText>
+              </FlexWrapper>
+            </IngredientsHeader>
+            <ul>
+              {ingredients.map(ingr => {
+                return (
+                  <IngredientItem key={ingr._id}>
+                    <FlexWrapper>
+                      {ingr.thb ? (
+                        <IngredientImg src={ingr.thb} alt={ingr.ttl} />
+                      ) : (
+                        <IngredientPlaceholder>
+                          <use href={placeholders + '#ph-apple-60px'} />
+                        </IngredientPlaceholder>
+                      )}
+                      <IngredientName>{ingr.ttl}</IngredientName>
+                    </FlexWrapper>
+                    <FlexWrapper>
+                      <IngredientNumber>{ingr.measure}</IngredientNumber>
+                      <label>
+                        <Checkbox
+                          ingrId={ingr._id}
+                          shopListProp={shopList}
+                          ingrMeasure={ingr.measure}
+                        />
+                      </label>
+                    </FlexWrapper>
+                  </IngredientItem>
+                );
+              })}
+            </ul>
+          </>
+        )}
       </Container>
     </IngredientsSection>
   );
