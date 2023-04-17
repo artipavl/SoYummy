35726b7fb1578108c2d8c7ca4177652c496af8c1
Andrey Yurchenko <spree92spree92@gmail.com>
2023-04-17 17:23:24 +0300

    fix selector

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -42,7 +42,7 @@ const SearchForm = ({ get }) => {
   const toggleDropdown = e => {
     setParamsType(e.target.value);
 
-    setOpen(!open);
+    setOpen(prevOpen => !prevOpen);
   };
 
   const handleSelectOption = e => {
@@ -60,7 +60,11 @@ const SearchForm = ({ get }) => {
   });
 
   window.addEventListener('mousedown', e => {
-    if (optionsRef.current && !optionsRef.current.contains(e.target)) {
+    if (
+      optionsRef.current &&
+      !optionsRef.current.contains(e.target) &&
+      e.target !== document.querySelector('[name="type"]')
+    ) {
       setOpen(false);
     }
   });
